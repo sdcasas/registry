22cf3f75cc70ff8c27b6544680676b6a412b9179
Sergio Casas <sdcasas@gmail.com>
2018-09-11 19:05:42 -0300

    Added configuration for static files

--- registry_project/config/settings.py
+++ registry_project/config/settings.py
@@ -124,3 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.abspath(BASE_DIR), 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "assets"),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.abspath(BASE_DIR), 'media')
+
+# permisos por defecto para los archivos subidos
+FILE_UPLOAD_PERMISSIONS = 0o755
